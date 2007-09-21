324285279f2123a82d57e14513380a3c87f1ecba
James Bunton <jamesbunton@fastmail.fm>
2007-09-21 12:21:32 +1000

    Added sendmail script

---
+++ mail/sendmail.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+# Specify SMTP servers here
+smtpServerList = [
+(".internode.on.net", "mail.internode.on.net"),
+(".usyd.edu.au", "mail.usyd.edu.au"),
+(".iinet.net.au", "mail.iinet.net.au"),
+(".netspace.net.au", "mail.netspace.net.au"),
+(".optusnet.com.au", "mail.optusnet.com.au")
+]
+
+
+
+import email, email.utils, smtplib, sys, urllib
+
+# Get the to addresses
+toAddrs = sys.argv[1:]
+toAddrs.remove("--")
+if len(toAddrs) == 0 or filter(lambda to: to.startswith("-"), toAddrs):
+	# Either no addresses, or an unknown parameter
+	print >>sys.stderr, "Usage: %s toAddr ..." % sys.argv[0]
+	sys.exit(1)
+
+# Pick a SMTP server
+try:
+	host = urllib.urlopen("http://suits.ug.it.usyd.edu.au/myip.php").read().strip()
+	for hostMatch, smtpServer in smtpServerList:
+		if host.endswith(hostMatch):
+			# Got the correct smtpServer
+			break
+except Exception, e:
+	print >>sys.stderr, "Error! Couldn't pick an SMTP server"
+	print >>sys.stderr, e
+	sys.exit(1)
+
+# Get the from address
+message = sys.stdin.read()
+fromAddr = email.message_from_string(message)["from"]
+fromAddr = email.utils.parseaddr(fromAddr)[1]
+
+# Send the email
+smtp = smtplib.SMTP(smtpServer)
+smtp.sendmail(fromAddr, toAddrs, message)
+smtp.quit()
+
