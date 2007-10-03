03b06fa45f129969da1200c91ed4c366d4985b6c
James Bunton <jamesbunton@fastmail.fm>
2007-10-03 23:35:15 +1000

    maildirstat.py now uses kqueues!

--- mail/maildirstat.py
+++ mail/maildirstat.py
@@ -1,7 +1,42 @@
 #!/usr/bin/env python
 
 import os, os.path, sys
+import kqueue
 j = os.path.join
+sep = os.path.sep
+
+def watchDirectories(directories):
+	""" Given a list of directories to monitor, returns a function which when
+	called will sleep until a directory changes, and then return a list of
+	changed directories.
+	"""
+	def doWatch():
+		while True:
+			changed = []
+			for event in kqueue.kevent(kq, events, 10, None):
+				changed.append(directories[dirfds.index(event.ident)])
+			return changed
+
+	# Open all the directories
+	dirfds = []
+	for directory in directories:
+		dirfds.append(os.open(directory, os.O_RDONLY))
+
+	# Set up the kqueue events
+	events = []
+	filter = kqueue.EV_ADD | kqueue.EV_CLEAR | kqueue.EV_ENABLE
+	fflags = kqueue.NOTE_WRITE | kqueue.NOTE_DELETE | kqueue.NOTE_EXTEND
+	for fd in dirfds:
+		e=kqueue.Event(fd, kqueue.EVFILT_VNODE, filter, fflags=fflags, data=fd)
+		events.append(e)
+
+	# Give back a function pointer
+	kq = kqueue.kqueue()
+	return doWatch
+
+def clear():
+	""" Clears the screen """
+	os.system("clear")
 
 def checkMailbox(mailbox):
 	""" Ensure that mailbox is a maildir directory """
@@ -16,7 +51,8 @@ def extractFlags(messages):
 	""" Extract the flags from the messages """
 	return [m.rsplit(":", 1)[1].split(",")[1] for m in messages]
 
-def main(mailbox, format):
+def maildirStat(mailbox):
+	""" Returns newCount, unreadCount for the given maildir mailbox """
 	checkMailbox(mailbox)
 
 	# Reuse the directory listing
@@ -29,18 +65,45 @@ def main(mailbox, format):
 	# Get the unread messages
 	unreadCount = len([m for m in curList if "S" not in m])
 
-	# Format it nicely
-	format = format.replace("\\n", "\n")
-	mailbox = os.path.basename(mailbox)
-	sys.stdout.write(format % (mailbox, newCount, unreadCount))
+	return newCount, unreadCount
+
+def multiStat(mailboxes, counts):
+	for mailbox in mailboxes:
+		counts[mailbox] = maildirStat(mailbox)
+
+def printCounts(mailboxes, counts):
+	""" Prints the counts to the screen ordered """
+	clear()
+	for mailbox in mailboxes:
+		newCount, unreadCount = counts[mailbox]
+		mailbox = mailbox.rsplit("/", 1)[-1]
+		print "%s:\n\t%d\t%d\n" % (mailbox, newCount, unreadCount)
+
+def main(mailboxes):
+	""" Stat all the mailboxes and then watch for changes """
+	counts = {}
+	multiStat(mailboxes, counts)
+	printCounts(mailboxes, counts)
+
+	watcher = watchDirectories([j(m, "cur") for m in mailboxes] +
+	                           [j(m, "new") for m in mailboxes])
+	while True:
+		changed = watcher()
+		changed = [sep.join(d.split(os.path.sep)[:-1]) for d in changed]
+		changed = list(set(changed))
+		multiStat(changed, counts)
+		printCounts(mailboxes, counts)
+
 
 if __name__ == "__main__":
 	try:
-		mailbox = sys.argv[1]
-		format = sys.argv[2]
+		mailboxes = sys.argv[1:]
 	except IndexError:
-		print >> sys.stderr, "Usage: %s mailbox format" % sys.argv[0]
+		print >> sys.stderr, "Usage: %s mailbox [mailbox ...]" % sys.argv[0]
 		sys.exit(1)
 
-	main(mailbox, format)
+	try:
+		main(mailboxes)
+	except KeyboardInterrupt:
+		print
 
