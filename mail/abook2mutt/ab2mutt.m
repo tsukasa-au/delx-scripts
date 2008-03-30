cc5ac3d1091ed23bd27951f4e57424bc9f41511d
James Bunton <jamesbunton@fastmail.fm>
2008-03-30 20:46:37 +1100

    Improved ab2mutt heaps

--- mail/abook2mutt/ab2mutt.m
+++ mail/abook2mutt/ab2mutt.m
@@ -1,15 +1,46 @@
 /* Copyright 2008 James Bunton <jamesbunton@fastmail.fm>
  * Licensed for distribution under the GPL version 2.
  *
- * Dump the contents of the OSX address book in a format usable as by Mutt as
- * an alias file.
+ * Dump the contents of the OSX address book as a Mutt alias file
  */
 
 
 #import <Foundation/Foundation.h>
 #import <AddressBook/AddressBook.h>
 
-#define NSPrint(fmt, ...) [[NSString stringWithFormat:fmt, ## __VA_ARGS__] writeToFile:@"/dev/stdout" atomically:NO encoding:NSUTF8StringEncoding error:nil]
+
+void
+NSPrint(NSString* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	NSString* string = [[NSString alloc] initWithFormat:format arguments:args];
+	va_end(args);
+	[string writeToFile:@"/dev/stdout" atomically:NO encoding:NSUTF8StringEncoding error:nil];
+	[string release];
+}
+
+
+@interface NSString (james)
+-(NSString*) keepOnlyCharacterSet:(NSCharacterSet*)keepChars;
+@end
+@implementation NSString (james)
+-(NSString*)
+keepOnlyCharacterSet:(NSCharacterSet*)keepChars;
+{
+	NSMutableString* ret = [[NSMutableString alloc] init];
+	NSScanner* scanner = [NSScanner scannerWithString:self];
+	NSString* temp;
+	while(![scanner isAtEnd]) {
+		if([scanner scanCharactersFromSet:keepChars intoString:&temp]) {
+			[ret appendString:temp];
+		} else {
+			[scanner setScanLocation:1+[scanner scanLocation]];
+		}
+	}
+	return [ret autorelease];
+}
+@end
 
 int
 main()
@@ -20,26 +51,30 @@ main()
 		NSString* firstName = [[person valueForProperty:kABFirstNameProperty] description];
 		NSString* lastName = [[person valueForProperty:kABLastNameProperty] description];
 		NSString* nickName = [[person valueForProperty:kABNicknameProperty] description];
+		// WTF does ABMultiValue exist for? No fast enumeration? What's wrong with NSArray?!
+		ABMultiValue* emails = [person valueForProperty:kABEmailProperty];
 
-		if(!([firstName length] > 0 && [lastName length] > 0)) {
-			continue; // Ignore empty entries
+		if(!([firstName length] > 0 && [lastName length] > 0 && [emails count] > 0)) {
+			// Ignore empty entries
+			continue;
 		}
 
 		// Mutt requires a key
+		NSString* key;
 		if([nickName length] > 0) {
-			NSPrint(@"alias %@ ", nickName);
+			key = nickName;
 		} else {
-			NSPrint(@"alias %@%@ ", firstName, lastName);
+			key = [NSString stringWithFormat:@"%@%@", firstName, lastName];
 		}
+		key = [[key lowercaseString] keepOnlyCharacterSet:[NSCharacterSet lowercaseLetterCharacterSet]];
 
-		NSPrint(@"%@ %@ ", firstName, lastName);
-
-		// WTF does ABMultiValue exist for? No fast enumeration? What's wrong with NSArray?!
-		ABMultiValue* emails = [person valueForProperty:kABEmailProperty];
 		for(unsigned int i = 0; i < [emails count]; ++i) {
-			NSPrint(@"%@ ", [emails valueAtIndex:i]);
+			NSString* keySuffix = @"";
+			if(i > 0) {
+				keySuffix = [NSString stringWithFormat:@"%d", i];
+			}
+			NSPrint(@"alias %@%@ \"%@ %@\" <%@>\n", key, keySuffix, firstName, lastName, [emails valueAtIndex:i]);
 		}
-		NSPrint(@"\n");
 	}
 
 	[pool release];
