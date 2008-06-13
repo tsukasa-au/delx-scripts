c380338a0d4e75a5309e8743b191e3da78d8c213
James Bunton <jamesbunton@fastmail.fm>
2008-06-12 20:13:50 -0700

    AB2Mutt: Generate alias lines for nicknames and full names.

--- mail/abook2mutt/ab2mutt.m
+++ mail/abook2mutt/ab2mutt.m
@@ -42,6 +42,21 @@ keepOnlyCharacterSet:(NSCharacterSet*)keepChars;
 }
 @end
 
+
+void
+printMuttAlias(NSString* key, NSString* firstName, NSString* lastName, ABMultiValue* emails)
+{
+	key = [[key lowercaseString] keepOnlyCharacterSet:[NSCharacterSet lowercaseLetterCharacterSet]];
+
+	for(unsigned int i = 0; i < [emails count]; ++i) {
+		NSString* keySuffix = @"";
+		if(i > 0) {
+			keySuffix = [NSString stringWithFormat:@"%d", i];
+		}
+		NSPrint(@"alias %@%@ \"%@ %@\" <%@>\n", key, keySuffix, firstName, lastName, [emails valueAtIndex:i]);
+	}
+}
+
 int
 main()
 {
@@ -54,26 +69,17 @@ main()
 		// WTF does ABMultiValue exist for? No fast enumeration? What's wrong with NSArray?!
 		ABMultiValue* emails = [person valueForProperty:kABEmailProperty];
 
-		if(!([firstName length] > 0 && [lastName length] > 0 && [emails count] > 0)) {
-			// Ignore empty entries
+		if([emails count] == 0) {
 			continue;
 		}
 
-		// Mutt requires a key
-		NSString* key;
+		// Generate an entry for Nick and FirstLast
 		if([nickName length] > 0) {
-			key = nickName;
-		} else {
-			key = [NSString stringWithFormat:@"%@%@", firstName, lastName];
+			printMuttAlias(nickName, firstName, lastName, emails);
 		}
-		key = [[key lowercaseString] keepOnlyCharacterSet:[NSCharacterSet lowercaseLetterCharacterSet]];
-
-		for(unsigned int i = 0; i < [emails count]; ++i) {
-			NSString* keySuffix = @"";
-			if(i > 0) {
-				keySuffix = [NSString stringWithFormat:@"%d", i];
-			}
-			NSPrint(@"alias %@%@ \"%@ %@\" <%@>\n", key, keySuffix, firstName, lastName, [emails valueAtIndex:i]);
+		if([firstName length] > 0 && [lastName length] > 0) {
+			NSString* key = [NSString stringWithFormat:@"%@%@", firstName, lastName];
+			printMuttAlias(key, firstName, lastName, emails);
 		}
 	}
 
